8d6504da5001fea2f22414252b9c13cd78316362
Viktoriia Ugriumova <viktoriaugriumova@gmail.com>
2024-09-09 21:42:16 +0200

    all except listener

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+// DOM узлы
+const cardContainer = document.querySelector('.places__list');
+const deleteCardButton = document.querySelector('.card__delete-button');
 
-// @todo: DOM узлы
+//Функция удаления карточки
+function deleteCard() {
+    const card = deleteCardButton.closest('.places__item');
+    card.remove();
+};
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function renderCards(deleteCard) {
+    for (let i = 0; i < initialCards.length; i++) {
+        const cardTemplate = document.querySelector('#card-template').content;
+        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+        cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+        cardElement.querySelector('.card__image').src = initialCards[i].link;
 
-// @todo: Вывести карточки на страницу
+        cardContainer.append(cardElement);
+
+        // deleteCardButton.addEventListener('click', deleteCard);
+    }
+};
+
+// Вывести карточки на страницу
+renderCards();
