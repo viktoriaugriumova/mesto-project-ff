f5a160e33da48c14fae6ba5a236fa65f7fd79572
Viktoriia Ugriumova <viktoriaugriumova@gmail.com>
2024-09-14 22:02:29 +0200

    fixes after review

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Горы Архыз"
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Зимняя река в Челябинской области"
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Серые панельные дома"
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Гора на Камчатке"
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Железная дорога, идущая сквозь лес"
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Зимний обрыв над Байкалом"
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
 // DOM узлы
-const cardContainer = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 //Функция удаления карточки
 function deleteCard(event) {
     const card = event.target.closest('.places__item');
-    console.log(card);
     card.remove();
 };
 
-
 // Функция создания карточки
-function renderCards() {
-    for (let i = 0; i < initialCards.length; i++) {
-        const cardTemplate = document.querySelector('#card-template').content;
-        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+function createCard(newCard, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-        cardElement.querySelector('.card__title').textContent = initialCards[i].name;
-        cardElement.querySelector('.card__image').src = initialCards[i].link;
+    cardElement.querySelector('.card__title').textContent = newCard.name;
+    cardElement.querySelector('.card__image').src = newCard.link;
+    cardElement.querySelector('.card__image').alt = newCard.alt;
 
-        const deleteCardButton = cardElement.querySelector('.card__delete-button');
-        deleteCardButton.addEventListener('click', deleteCard);
+    const deleteCardButton = cardElement.querySelector('.card__delete-button');
+    deleteCardButton.addEventListener('click', deleteCard);
 
-        cardContainer.append(cardElement);
-    }
+    return cardElement;
 };
 
 // Вывести карточки на страницу
+function renderCards() {
+    for (let i = 0; i < initialCards.length; i++) {
+        const newCard = createCard(initialCards[i], deleteCard);
+        cardsContainer.append(newCard);
+    }
+};
+
 renderCards();
