81d20b632a79cad5901de747825b4f3ee4de5b5d
Viktoriia Ugriumova <viktoriaugriumova@gmail.com>
2024-10-18 19:17:28 +0200

    снимаю овердей в закрытии

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -14,6 +14,7 @@ export function closePopup(popupElement) {
 	popupElement.classList.remove('popup_is-opened')
     document.removeEventListener('click', closeOnCross);
     document.removeEventListener('keydown', closeOnEsc);
+    document.removeEventListener('click', closeOnOverlay);
 }
 
 // Закрытие на оверлей
